880b0a65cefc17b2d54fc26ccce4f89db025b902
cmis <cmis@cmis-hs18.(none)>
2016-02-01 13:34:21 +0700

    caught up with work

---
+++ first.py
@@ -0,0 +1,22 @@
+myname = "Pim"
+myage = 15 + (17+30+31+30+31+31+1) / 365.0
+myheight = 1.75
+side1square = 4
+lenghtrect = 6
+heightrect = 9
+myagemonths = myage + 17
+lifeexpectancy = 74.72
+daysleft = lifeexpectancy - myage
+feet = 3.28084
+heightfeet = myheight * feet
+personage = 15 + (17+30+31+30+31+31+1) / 365.0
+personsex = "female"
+personheight = 1.67
+myaverage = myheight - personheight
+areasquare = side1square ** 2
+volumesquare = side1square * side1square * side1square / 2
+areaninthinrec = (lenghtrect * heightrect) / 9
+print "Hello, my name is", myname. "I am", my age, "years old". "My height is", 
+
+
+
