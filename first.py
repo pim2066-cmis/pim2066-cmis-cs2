3f057658fda4c358854a6392607ceceb3f23efd9
cmis <cmis@cmis-hs18.(none)>
2016-02-03 08:19:12 +0700

    finished working

--- first.py
+++ first.py
@@ -1,22 +1,49 @@
 myname = "Pim"
+#this variable is for my name
 myage = 15 + (17+30+31+30+31+31+1) / 365.0
+#this variable is for the exact of my age
 myheight = 1.75
+#this variable is for my height
 side1square = 4
+#this variable is for one side of a square
 lenghtrect = 6
+#this variable is for the lenght of the rectangle
 heightrect = 9
+#this variable is for the height of the rectangle
 myagemonths = myage + 17
+#this variable is for my age in months
 lifeexpectancy = 74.72
+#this variable is for the life expectancy in thailand
 daysleft = lifeexpectancy - myage
+#this variable is for the days lef in my life
 feet = 3.28084
+#this variable is for one feet in one meter
 heightfeet = myheight * feet
+#this variable is for my height in feet
 personage = 15 + (17+30+31+30+31+31+1) / 365.0
+#this variable is for a person my age
 personsex = "female"
+#this variable is for a person's gender
 personheight = 1.67
+#this variable is for a person's height
 myaverage = myheight - personheight
+#this variable is for my average height
 areasquare = side1square ** 2
+#this variable is for the area of a square
 volumesquare = side1square * side1square * side1square / 2
+#this variable is for the volume of a square
 areaninthinrec = (lenghtrect * heightrect) / 9
-print "Hello, my name is", myname. "I am", my age, "years old". "My height is", 
+#this variable is for the one-ninth area of a rectangle
+
+print "Hello, my name is " + str(myname) +"." + " and I am " + str(myage) + " years old " + "." + " That means that I have about " + str(daysleft) + "from the life expectancy of Thailand, which is " + str(lifeexpectancy) + "." + " My height is " + str(myheight) + " which is about " + str(myaverage) + " taller than the thai girls my age."
+
+print "The unit of a cube is ", side1square, " inches long.", " The area of one side of the cube is ", areasquare, ".", " The volume of the square would be ", volumesquare, ". We have a rectangle that have the height of ", heightrect, " inches and the lenght of ", lenghtrect, ". The one-ninth of the rectangle is ", areaninthinrec, "." 
+
+smile = ":)" * 10000
+#this variable is for 10000 :) 
+
+print smile
+
 
 
 
