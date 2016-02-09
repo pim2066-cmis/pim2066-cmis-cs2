c821c29cc833516e151f74ac9c2971959199cec1
cmis <cmis@cmis-hs18.(none)>
2016-02-09 08:39:57 +0700

    working

---
+++ functions.py
@@ -0,0 +1,19 @@
+def add(a, b):	        
+	return a + b	    
+			    
+c = add(3,7)		  
+
+
+def sub(a, b):
+	return a - b
+
+d = sub(9, 6)
+
+print
+
+def mul(a, b):
+	return a * b
+
+e = mul(9,66)
+
+
