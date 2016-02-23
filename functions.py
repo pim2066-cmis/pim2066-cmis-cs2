f98cc12711e37497fa9b71d7d26ef25bc8a8d2e2
cmis <cmis@cmis-hs18.(none)>
2016-02-23 08:38:42 +0700

    DONE

--- functions.py
+++ functions.py
@@ -1,45 +1,38 @@
 import math
 def add(a, b):	        
 	return a + b	    		    
-
-print  add(3,7)
+print  add(3,4)
 
 
 def sub(a, b):
 	return a - b
-
 print sub(9, 6)
 
 def mul(a, b):
 	return a * b
-
 print  mul(9,66)
 
 def div(a, b):
 	return a/b
-
 print div(9, 3)
 
 def	sec(seconds, secondsinhr):
 	return seconds/secondsinhr
-sec(86400, 3600)
 print sec(86400, 3600) 
 
 def ra(radius, radius1, pi):
 	return radius * radius1 * pi
-ra(5, 5, 3.14159265359)
 print ra(5, 5, 3.14159265359)
 
 def pi_area(fourth, pi, radius, radius1, radius2):
 	return fourth * pi * radius * radius * radius
-pi_area(4/3, 3.14159265359, 6, 6, 6)
 print pi_area(4/3, 3.14159265359, 6, 6, 6)
 
 def avg_volume (a, b):
 	 return ((1.0/6 * math.pi * a**3) + (1.0/6 * math.pi * b**3)) /2
 print avg_volume(10, 20)
 
-def div(a, b, c, half):
+def tri_area(a, b, c, half):
 	return ((a + b + c) / half)
 print tri_area(1, 2, 2.5, 2)
 
@@ -61,7 +54,7 @@ def msg(word):
 print msg("Hello")
 print msg("I eat cats!")
 
-a = add(3,7)
+a = add(3,4)
 b = sub(9, 6)
 c = mul(9,66)
 d = div(9, 3)
@@ -73,19 +66,44 @@ i = areacir(1,2,2.5)
 j = ra(5, 5, 3.14159265359)
 k = hi_right("Hello")
 l = hi_mid("Hello")
+m = add(3,4)
+n = sub(9, 6)
+o = mul(9,66)
+p = div(9, 3)
+q = sec(86400, 3600)
+r = tri_area(1, 2, 2.5, 2)
+s = pi_area(4/3, 3.14159265359, 6, 6, 6)
+t = avg_volume(10, 20)
+u = areacir(1,2,2.5)
+v = ra(5, 5, 3.14159265359)
+w = hi_right("Hello")
+x = hi_mid("Hello")
 
-print msg_box(str(a))
-print msg_box(str(b))
-print msg_box(str(c))
-print msg_box(str(d))
-print msg_box(str(e))
-print msg_box(str(f))
-print msg_box(str(g))
-print msg_box(str(h))
-print msg_box(str(i))
-print msg_box(str(j))
-print msg_box(str(k))
 
+print msg(str(a))
+print msg(str(b))
+print msg(str(c))
+print msg(str(d))
+print msg(str(e))
+print msg(str(f))
+print msg(str(g))
+print msg(str(h))
+print msg(str(i))
+print msg(str(j))
+print msg(str(k))
+print msg(str(l))
+print msg(str(m))
+print msg(str(n))
+print msg(str(o))
+print msg(str(p))
+print msg(str(q))
+print msg(str(r))
+print msg(str(s))
+print msg(str(t))
+print msg(str(u))
+print msg(str(v))
+print msg(str(w))
+print msg(str(x))
 
 
 
--- simple.py
+++ simple.py
@@ -1,10 +1,12 @@
 import math
 def solve_qua_func(a , b, c):
-	x1 = ( - b + math.sqrt ( ( b * b ) - ( 4 * a * c ) ) / 2 * a )
-	return x1
-def solve_qua_func(a , b, c):
-	x2 = ( - b - math.sqrt ( ( b * b ) - ( 4 * a * c ) ) / 2 * a )
-	return x2
+	return (( -b ) +  math.sqrt (( b*b ) - ( 4*a*c ))) / ( 2*a )
+def solve_qua_func1(a , b, c):
+	return (( -b ) -  math.sqrt (( b*b ) - ( 4*a*c ))) / ( 2*a )
+
+def output(name, a, b, c, x1, x2):
+	return """{}, the equation that you've given was {}x(square) + {}x + {} = 0
+	After the calculation, the answer for this is x = {}, {}""".format(name, a, b, c, x1, x2)
 
 
 def main():
@@ -12,13 +14,10 @@ def main():
 	a = raw_input("Value of a: ")
 	b = raw_input("Value of b: ")
 	c = raw_input("value of c: ")
-	x1 = mul(int(a), int(b), int(c))
-	x2 = mul1(int(a), int(b), int(c))
-	print output(hello, name, a, b, c, x1, x2)
+	x1 = solve_qua_func(int(a), int(b), int(c))
+	x2 = solve_qua_func1(int(a), int(b), int(c))
+	print output(name, a, b, c, x1, x2)
 
-def output(name, a, b, c, x1, x2):
-	return """{}, the equation that you've given was {}x(square) + {}x + {} = 0
-	After the calculation, the answer for this is x = {}, {}""".format(name, a, b, c, x1, x2)
 
 main()
 	
