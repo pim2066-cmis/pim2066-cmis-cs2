71da3ff3cac424876e9fddc0d8696a0fdefdc70f
cmis <cmis@cmis-hs08.(none)>
2016-02-29 14:55:54 +0700

    Added

--- functions.py
+++ functions.py
@@ -20,19 +20,19 @@ def	sec(seconds, secondsinhr):
 	return seconds/secondsinhr
 print sec(86400, 3600) 
 
-def ra(radius, radius1, pi):
+def area_cir(radius, radius1, pi):
 	return radius * radius1 * pi
-print ra(5, 5, 3.14159265359)
+print area_cir(5, 5, 3.14159265359)
 
-def pi_area(radius):
+def sph_area(radius):
 	return 4/3.0 * math.pi * radius**3
-print pi_area(5)
+print sph_area(5)
 
-def avg_volume (d1,d2):
+def avg_volume_sph (d1,d2):
     vs1 = pi_area(d1/2)
     vs2 = pi_area(d2/2)
     return (vs1+vs2)/2
-print avg_volume
+print avg_volume_sph
 
 def tri_area(a, b, c, half):
 	return ((a + b + c) / half)
@@ -62,10 +62,10 @@ c = mul(9,66)
 d = div(9, 3)
 e = sec(86400, 3600)
 f = tri_area(1, 2, 2.5, 2)
-g = pi_area(4/3, 3.14159265359, 6, 6, 6)
-h = avg_volume(10, 20)
+g = sph_area(5)
+h = avg_volume_sph(10, 20)
 i = areacir(1,2,2.5)
-j = ra(5, 5, 3.14159265359)
+j = area_cir(5, 5, 3.14159265359)
 k = hi_right("Hello")
 l = hi_mid("Hello")
 m = add(3,4)
@@ -74,10 +74,10 @@ o = mul(9,66)
 p = div(9, 3)
 q = sec(86400, 3600)
 r = tri_area(1, 2, 2.5, 2)
-s = pi_area(4/3, 3.14159265359, 6, 6, 6)
-t = avg_volume(10, 20)
+s = sph_area(5)
+t = avg_volume_sph(10, 20)
 u = areacir(1,2,2.5)
-v = ra(5, 5, 3.14159265359)
+v = area_cir(5, 5, 3.14159265359)
 w = hi_right("Hello")
 x = hi_mid("Hello")
 
