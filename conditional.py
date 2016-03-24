a23f58174332dcca3472c736febc776b80a1d4b6
cmis <cmis@cmis-hs02.(none)>
2016-03-24 08:40:16 +0700

    not done

---
+++ conditional.py
@@ -0,0 +1,29 @@
+def intro(introduction):
+	introduction = raw_input("This game will let you choose your own path to become a warrior. You will start off as a kittypet (house cat) that will choose whether to go to the forest or stay in bed. You will face obstacles along the game and will need to make your decision carefully. Will you survive to become the most powerful leader in the forest? Or will you die before becoming an elder? Goodluck and may Starclan light up your path.")
+
+def wakes_up():
+	in_bed = ("""
+You wake up on your owner's bed and wonder what to do.
+a. Go to the forest
+b. Begs for food
+c. Stay in bed
+(Choose a , b, or c): """)
+
+	if wakes_up == "a"
+		wakes_up = """You explore the forest and find a big group of cats made up of four cats. A young light brown she-cat, A massive black tom, A gray and white she-cat, and a young Black and brown Tom.
+
+
+"What do you want?!" the gray and white she-cat hissed, claws out.
+
+
+"Stop, Lilypetal" the Black and brown tom meowed
+
+
+"Do you want to join?" the young light brown she-cat meowed""" 
+	else: wakes_up == "b"
+		return 
+	else wakes_up == "c"
+		return 3
+
+
+
