feb0a244df634e8b928522ca19917ce8aad4fda9
cmis <cmis@cmis-hs07.(none)>
2016-03-28 10:55:19 +0700

    Not done

--- conditional.py
+++ conditional.py
@@ -1,16 +1,5 @@
-def intro(introduction):
-	introduction = raw_input("This game will let you choose your own path to become a warrior. You will start off as a kittypet (house cat) that will choose whether to go to the forest or stay in bed. You will face obstacles along the game and will need to make your decision carefully. Will you survive to become the most powerful leader in the forest? Or will you die before becoming an elder? Goodluck and may Starclan light up your path.")
-
-def wakes_up():
-	in_bed = ("""
-You wake up on your owner's bed and wonder what to do.
-a. Go to the forest
-b. Begs for food
-c. Stay in bed
-(Choose a , b, or c): """)
-
-	if wakes_up == "a"
-		wakes_up = """You explore the forest and find a big group of cats made up of four cats. A young light brown she-cat, A massive black tom, A gray and white she-cat, and a young Black and brown Tom.
+def meet_cats():
+       return """You explore the forest and find a big group of cats made up of four cats. A young light brown she-cat, A massive black tom, A gray and white she-cat, and a young Black and brown Tom.
 
 
 "What do you want?!" the gray and white she-cat hissed, claws out.
@@ -19,11 +8,41 @@ c. Stay in bed
 "Stop, Lilypetal" the Black and brown tom meowed
 
 
-"Do you want to join?" the young light brown she-cat meowed""" 
-	else: wakes_up == "b"
-		return 
-	else wakes_up == "c"
-		return 3
+"Do you want to join?" the young light brown she-cat meowed"""
+
+
+def intro(introduction):
+	introduction = raw_input("This game will let you choose your own path to become a warrior. You will start off as a kittypet (house cat) that will choose whether to go to the forest or stay in bed. You will face obstacles along the game and will need to make your decision carefully. Will you survive to become the most powerful leader in the forest? Or will you die before becoming an elder? Goodluck and may Starclan light up your path.")
+
+def wakes_up():
+	in_bed = ("""
+You wake up on your owner's bed and wonder what to do.
+    Go to the forest
+    Begs for food
+    Stay in bed
+Type in one of the choice: """)
+
+	if wakes_up == "Go to the Forest":
+		return meet_cats
+
+	elif wakes_up == "Begs for food":
+		return choose("""You beg your owner for food and it goes back to sleep.
+
+
+What to do now?
+    Go to the Forest
+    Go Back to Sleep
+""")
+		if choose == "Go to the Forest":
+			def meet_cats():
+
+	    else wakes_up == "Go Back to Sleep":
+            return """You go back to sleep and never join a clan and enjoy your dream. 
+
+End Game 
+"""
+
+		
 
 
 
--- simple.py
+++ simple.py
@@ -10,10 +10,10 @@ def output(name, a, b, c, x1, x2):
 
 
 def main():
-	name = raw_input("HELLOO!!! What's your name, pal?: ")
-	a = raw_input("Value of a: ")
-	b = raw_input("Value of b: ")
-	c = raw_input("value of c: ")
+	name = raw_input("HELLOO!!!This is a calculator which will find the value of x in quadratic function for you.")
+	a = raw_input("Value of a (Number no descimals): ")
+	b = raw_input("Value of b (Number no descimals): ")
+	c = raw_input("value of c (Number no descimals): ")
 	x1 = solve_qua_func(int(a), int(b), int(c))
 	x2 = solve_qua_func1(int(a), int(b), int(c))
 	print output(name, a, b, c, x1, x2)
