1244ae9b132f98ac93dd81f00346cd334121e6e5
cmis <cmis@cmis-hs07.(none)>
2016-04-27 10:10:33 +0700

    Working

--- recursion.py
+++ recursion.py
@@ -1,9 +1,9 @@
-def countup(start,stop):
+"""def countup(start,stop):
 	if start >= stop:
 		print "BOOM!!"
 	else:
 		print start
-		countup(start+1)
+		countup(start+1,stop)
 
 def main():
 	start = int(raw_input("Start: "))
@@ -13,3 +13,54 @@ def main():
 	return
 
 main()
+
+
+
+def countup(start,stop):
+	if start <= stop:
+		print "BOOM!!"
+	else:
+		print start
+		countup(start-1,stop)
+
+def main():
+	start = int(raw_input("Start: "))
+	stop = int(raw_input("Stop: "))
+
+	countup(start,stop)
+	return
+
+main()"""
+
+
+
+
+def cal(run, next_no):
+	if total != "":
+		run(total)
+	else:
+		print run
+		cal(run + next_no)
+
+def main():
+	total = 0.0
+	next = 0.0
+
+	next_no =  float(raw_input("Running total: "))
+	if next_no == " ":
+		print cal()
+
+
+	run = float(raw_input("Running total: "))
+	if run == int():
+		return cal()
+	elif run == None:
+		print "Good BYE"
+	else:
+		print float(raw_input("Running total: "))
+
+	
+
+
+main()
+	
