099a348bd3e17af3fed2c0ca6fc3a359c1f1f10c
cmis <cmis@cmis-hs07.(none)>
2016-04-28 13:35:15 +0700

    BRAIN

--- recursion.py
+++ recursion.py
@@ -30,37 +30,53 @@ def main():
 	countup(start,stop)
 	return
 
-main()"""
-
-
-
-
-def cal(run, next_no):
-	if total != "":
-		run(total)
-	else:
-		print run
-		cal(run + next_no)
-
-def main():
-	total = 0.0
-	next = 0.0
-
-	next_no =  float(raw_input("Running total: "))
-	if next_no == " ":
-		print cal()
-
-
-	run = float(raw_input("Running total: "))
-	if run == int():
-		return cal()
-	elif run == None:
-		print "Good BYE"
-	else:
-		print float(raw_input("Running total: "))
-
-	
-
-
 main()
-	
+
+
+def adder2(running_total = 0):
+	print "The running total is {}.".format(running_total)
+	number = raw_input("Next Number: ")
+	if number != "":
+		running_total += float(number)
+		adder2(running_total) 
+	else:
+		print "The sum is {}.".format(running_total)
+adder2()
+
+
+def adder2(running_total = 0):
+	print "The running total is {}.".format(running_total)
+	number = raw_input("Next Number: ")
+	if number != "":
+		running_total += float(number)
+		adder2(running_total) 
+	else:
+		print "The sum is {}.".format(running_total)
+adder2()
+
+
+
+def biggest(big = 0):
+	number_1 = raw_input("Next: ")
+	if number_1 == "":
+		print "The biggest number is {}.".format(big)
+	elif float(number_1) < float(big):
+		biggest(big)
+	elif float(number_1) > float(big):
+		big = number_1
+		biggest(big)
+
+biggest()"""
+
+
+def biggest(big = float('inf')):
+	number_1 = raw_input("Next: ")
+	if number_1 == "":
+		print "The biggest number is {}.".format(big)
+	elif float(number_1) < float(big):
+		biggest(big)
+	elif float(number_1) > float(big):
+		big = number_1
+		biggest(big)
+
+biggest()
