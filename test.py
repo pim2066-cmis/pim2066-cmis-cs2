e5f191d2e4bddae64804dc2f9afc1209c7e5313d
cmis <cmis@cmis-hs07.(none)>
2016-05-24 13:32:41 +0700

    looping

---
+++ test.py
@@ -0,0 +1,27 @@
+#while True:
+#	print ":D"
+
+while False:	
+	print ":c"
+
+
+def countdown(x):
+	while x > 0:
+		print x
+		x -= 1
+
+def main():
+	countdown(9)
+
+main()
+
+
+def up(x):
+	while x < 10:
+		print x
+		x += 1
+
+def main():
+	up(0)
+
+main()
