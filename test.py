c77b26b7db03854ff7dccaa98aec5687d600dee9
cmis <cmis@cmis-hs07.(none)>
2016-05-26 08:37:57 +0700

    error

--- test.py
+++ test.py
@@ -1,27 +1,72 @@
 #while True:
 #	print ":D"
 
-while False:	
-	print ":c"
+#while False:	
+#	print ":c"
 
 
-def countdown(x):
-	while x > 0:
-		print x
-		x -= 1
+#def countdown(x):
+#	while x > 0:
+#		print x
+#		x -= 1
 
-def main():
-	countdown(9)
+#def main():
+#	countdown(9)
 
-main()
+#main()
+
+#
+#def up(x):
+#	while x < 10:
+#		print x
+#		x += 1
+
+#def main():
+#	up(0)
+
+#main()
 
 
-def up(x):
-	while x < 10:
-		print x
-		x += 1
+#def count(z,u):	
+#	if z < u:
+#		while z < u:
+#			print z
+#			z += 1
+#	elif z > u:
+#		while z > u:
+#			print z
+#			z -= 1
+#	while u > z:
+#		print u
+#		u -= 1
+#count(11,6)
 
-def main():
-	up(0)
+def add(a):
+	if a > 0:
+		while a > 0:
+			a += 1
+			if a % 2 != 0:
+				while a % 2 == 0:
+					a += a
+					print a
+	elif a < 0:
+		while a < 0:
+			a -= 1
+			if a % 2 != 0:
+				while a % 2 != 0:
+					a -= a
+					print a
+add(90)
 
-main()
+				
+
+
+
+
+
+
+
+
+
+
+
